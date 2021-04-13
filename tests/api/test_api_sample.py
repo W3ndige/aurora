bcfcf3d1ea110f03185f5f2a384bd0575bb09bb0
W3ndige <w3ndige@gmail.com>
2021-04-13 23:50:09 +0200

    Tests for sample API

--- tests/api/test_api_sample.py
+++ tests/api/test_api_sample.py
@@ -1,4 +1,5 @@
-from tests.utils import upload_file
+from tests.utils import upload_file, add_minhash, add_string
+
 
 def test_add_sample(client, monkeypatch):
     def mock_push_sample(a, b, c):
@@ -16,3 +17,78 @@ def test_add_sample(client, monkeypatch):
     assert response["filename"] == file["filename"]
     assert response["sha256"] == file["sha256"]
     assert response["filesize"] == len(file["content"])
+
+
+def test_get_sample(client):
+    file = upload_file()
+
+    response = client.get(f"/api/v1/sample/{file['sha256']}").json()
+
+    assert response["filename"] == file["filename"]
+    assert response["sha256"] == file["sha256"]
+    assert response["filesize"] == len(file["content"])
+
+
+def test_add_minhash(client):
+    file = upload_file()
+    minhash = add_minhash()
+    minhash.update({"minhash_type": "strings"})
+
+    response = client.post(f"api/v1/sample/{file['sha256']}/minhash", json=minhash).json()
+
+    assert response["minhash_type"] == minhash["minhash_type"]
+    assert response["seed"] == minhash["seed"]
+    assert response["hash_values"] == minhash["hash_values"]
+
+
+def test_get_sample_minhashes(client):
+    file = upload_file()
+
+    response = client.post(f"api/v1/sample/{file['sha256']}/minhash").json()
+
+    assert len(response) == 1
+
+
+def test_add_minhash_unknown_sample(client):
+    # Unknown file
+    file = upload_file(content="Unknown content")
+    minhash = add_minhash()
+    minhash.update({"minhash_type": "strings"})
+
+    response = client.post(f"api/v1/sample/{file['sha256']}/minhash", json=minhash)
+
+    assert response.status_code == 404
+
+
+def test_get_ssdeep(client):
+    file = upload_file()
+
+    response = client.get(f"/api/v1/sample/{file['sha256']}/ssdeep").json()
+
+    assert response["ssdeep"] == file["ssdeep"]
+
+
+def test_add_string(client):
+    file = upload_file()
+    string = add_string()
+
+    response = client.post(f"api/v1/sample/{file['sha256']}/string", json=string).json()
+
+    assert response["value"] == string["value"]
+    assert response["sha256"] == string["sha256"]
+    assert response["heuristic"] == string["heuristic"]
+
+
+def test_add_string_incorrect_sha256(client):
+    file = upload_file()
+    string = add_string()
+    modified_string = add_string(string="Modified string")
+
+    modified_string["value"] = string["value"]
+
+    response = client.post(f"api/v1/sample/{file['sha256']}/string", json=modified_string).json()
+
+    assert response["value"] == string["value"]
+    assert response["sha256"] == string["sha256"]
+    assert response["heuristic"] == string["heuristic"]
+
--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,9 @@
+import os
+import ssdeep
 import hashlib
-from typing import Dict
+import datasketch
+
+from typing import Dict, Any
 
 def upload_file(filename: str = None, content: str = None) -> Dict[str, str]:
     if not filename:
@@ -8,6 +12,10 @@ def upload_file(filename: str = None, content: str = None) -> Dict[str, str]:
     if not content:
         content = "content"
 
+    ssdeep_object = ssdeep.Hash()
+    ssdeep_object.update(content.encode("utf-8"))
+    ssdeep_hash = ssdeep_object.digest()
+
     sha256_object = hashlib.sha256()
     sha256_object.update(content.encode("utf-8"))
     sha256 = sha256_object.hexdigest()
@@ -15,7 +23,39 @@ def upload_file(filename: str = None, content: str = None) -> Dict[str, str]:
     return {
         "filename": filename,
         "content": content,
-        "sha256": sha256
+        "sha256": sha256,
+        "ssdeep": ssdeep_hash
     }
 
+
+def add_minhash() -> Dict[str, Any]:
+    minhash = datasketch.MinHash()
+
+    random_bytes = os.urandom(256)
+    minhash.update(random_bytes)
+
+    lean_minhash = datasketch.LeanMinHash(minhash)
+
+    return {
+        "seed": lean_minhash.seed,
+        "hash_values": lean_minhash.hashvalues.tolist()
+    }
+
+
+def add_string(string: str = None, heuristic: str = None) -> Dict[str, str]:
+    if not string:
+        string = "string"
+
+    if not heuristic:
+        heuristic = "heuristic"
+
+    sha256_object = hashlib.sha256()
+    sha256_object.update(string.encode("utf-8"))
+    sha256 = sha256_object.hexdigest()
+
+    return {
+        "value": string,
+        "sha256": sha256,
+        "heuristic": heuristic
+    }
 
